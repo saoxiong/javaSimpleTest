1d7e61cff09fb314352432c7b361bff79efe127d
saoxiong <saoxiongjiayou@gmail.com>
2013-12-15 14:04:34 +0800

    java2 file +

--- java2.java
+++ java2.java
@@ -2,5 +2,6 @@ public class Test2{
     private static void main(String[] agrs){
 	   System.out.println("Hello,Test2");
            System.out.println("Hi,there");
+	   System.out.println("Reflection2");
 	}
 }
